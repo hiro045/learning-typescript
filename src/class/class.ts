f542fcce873e5b147d3ceae899a97cf4945d75f5
Takashi Hirokane <hirokanetakashi@gmail.com>
2026-03-21 17:06:08 +0900

    Class getter, setter

--- src/class/class.ts
+++ src/class/class.ts
@@ -12,12 +12,25 @@ class Person {
 }
 
 class Teacher extends Person{
-    constructor(name: string, age: number, public subject: string) {
+    get subject(): string {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        this._subject = value;
+    }
+    constructor(name: string, age: number, private _subject: string) {
         super(name, age);
     }
     greeting() {
         console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}.`);
     }
 }
-const teacher = new Teacher("Taka", 40, "Math");
+const teacher = new Teacher('Taka', 40, 'Math');
+teacher.subject = 'Music';
 teacher.greeting();
